10e2e50ac9584cd2dc3b14ec044c4a6e58a7d792
ksgadiya1 <113256702+ksgadiya1@users.noreply.github.com>
2024-03-11 23:38:09 +0530

    update

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -19,7 +19,7 @@ import { useUploadThing } from '@/lib/uploadthing'
 import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "../ui/checkbox"
 import { useRouter } from "next/navigation"
-import { createEvent, updateEvent } from "@/lib/action/event.actions"
+import { createEvent, updateEvent } from "@/lib/actions/event.actions"
 import { IEvent } from "@/lib/database/models/event.model"
 
 
